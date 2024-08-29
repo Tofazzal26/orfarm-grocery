8af0d0dab7fc10c0621d3f59d77b36b961fa867a
Tofazzal26 <afranislamabir6789@gmail.com>
2024-08-30 01:24:20 +0600

    login ang register some code change

--- app/api/login/page.jsx
+++ app/api/login/page.jsx
@@ -14,10 +14,12 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center my-[100px]">
+    <div className="flex justify-center items-center my-[20px] md:my-[100px]">
       <div className="border-2 rounded-md">
-        <div className="px-[60px] pb-14">
-          <h1 className="text-center text-xl my-10">LOGIN</h1>
+        <div className="md:px-[60px] px-[20px] pb-[20px] md:pb-14">
+          <h1 className="text-center  text-lg md:text-xl my-4 md:my-10">
+            LOGIN
+          </h1>
           <form onSubmit={handleSubmit}>
             <div>
               <label className="text-gray-500">
@@ -27,7 +29,7 @@ const Login = () => {
               <input
                 type="text"
                 name="email"
-                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
+                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-full md:w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
               />
             </div>
             <div className="relative">
@@ -36,7 +38,7 @@ const Login = () => {
               <input
                 type={showPassword ? "text" : "password"}
                 name="password"
-                className="md:py-[10px] py-2 mt-2 px-3 w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
+                className="md:py-[10px] py-2 mt-2 px-3 w-full md:w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
               />
               {showPassword ? (
                 <span
--- app/api/register/page.jsx
+++ app/api/register/page.jsx
@@ -16,10 +16,12 @@ const Register = () => {
   };
 
   return (
-    <div className="flex justify-center items-center my-[100px]">
+    <div className="flex justify-center items-center my-[20px] md:my-[100px]">
       <div className="border-2 rounded-md">
-        <div className="px-[60px] pb-14">
-          <h1 className="text-center text-xl my-10 uppercase">Register</h1>
+        <div className="md:px-[60px] px-[20px] pb-[20px] md:pb-14">
+          <h1 className="text-center text-lg md:text-xl my-4 md:my-10 uppercase">
+            Register
+          </h1>
           <form onSubmit={handleSubmit}>
             <div>
               <label className="text-gray-500">Username *</label>
@@ -27,7 +29,7 @@ const Register = () => {
               <input
                 type="text"
                 name="name"
-                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
+                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-full md:w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
               />
             </div>
             <div>
@@ -36,7 +38,7 @@ const Register = () => {
               <input
                 type="text"
                 name="email"
-                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
+                className="md:py-[10px] py-2 mt-2 mb-4 px-3 w-full md:w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
               />
             </div>
             <div className="relative">
@@ -45,7 +47,7 @@ const Register = () => {
               <input
                 type={showPassword ? "text" : "password"}
                 name="password"
-                className="md:py-[10px] py-2 mt-2  px-3 w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
+                className="md:py-[10px] py-2 mt-2  px-3 w-full md:w-[400px] md:px-5 bg-[#f3f4f7] border-[1px]  outline-none rounded-none"
               />
               {showPassword ? (
                 <span
