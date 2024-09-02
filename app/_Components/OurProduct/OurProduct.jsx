7043420dd75f1d45f5ca03d40682fd64f2c20494
Tofazzal26 <afranislamabir6789@gmail.com>
2024-09-03 00:53:04 +0600

    Our Product toggle button style finish

--- app/_Components/OurProduct/OurProduct.jsx
+++ app/_Components/OurProduct/OurProduct.jsx
@@ -1,4 +1,11 @@
+"use client";
+
+import { Apple, CakeSlice, Cherry, Fish, Salad } from "lucide-react";
+import { useState } from "react";
+
 const OurProduct = () => {
+  const [toggle, setToggle] = useState(0);
+
   return (
     <div>
       <div className="text-center mt-[100px]">
@@ -8,7 +15,55 @@ const OurProduct = () => {
           requirements.
         </p>
       </div>
-      <div></div>
+      <div>
+        <div className="my-[50px] flex justify-center items-center">
+          <button
+            onClick={() => setToggle(0)}
+            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+              toggle === 0 && "border-[#80b500] text-[#80b500] border-b-2"
+            }`}
+          >
+            FOOD & DRINKS
+          </button>
+          <Apple className="text-gray-400 pb-3" size={32} />
+          <button
+            onClick={() => setToggle(1)}
+            className={`text-[18px]   pb-3 justify-center w-[200px] ${
+              toggle === 1 && "border-[#80b500] text-[#80b500] border-b-2"
+            }`}
+          >
+            VEGETABLES
+          </button>
+          <Salad className="text-gray-400 pb-3 " size={32} />
+          <button
+            onClick={() => setToggle(2)}
+            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+              toggle === 2 && "border-[#80b500] text-[#80b500] border-b-2"
+            }`}
+          >
+            DRIED FOODS
+          </button>
+          <Cherry className="text-gray-400 pb-3" size={32} />
+          <button
+            onClick={() => setToggle(3)}
+            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+              toggle === 3 && "border-[#80b500] text-[#80b500] border-b-2"
+            }`}
+          >
+            BREAD & CAKE
+          </button>
+          <CakeSlice className="text-gray-400 pb-3" size={32} />
+          <button
+            onClick={() => setToggle(4)}
+            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+              toggle === 4 && "border-[#80b500] text-[#80b500] border-b-2"
+            }`}
+          >
+            FISH & MEAT
+          </button>
+          <Fish className="text-gray-400 pb-3" size={32} />
+        </div>
+      </div>
     </div>
   );
 };
