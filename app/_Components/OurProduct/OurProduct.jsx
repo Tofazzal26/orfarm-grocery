fb192151ddb0ecfbf3c1c00f07d7e6b9e844126d
Tofazzal26 <afranislamabir6789@gmail.com>
2024-09-03 16:11:38 +0600

    our product responsibe finish

--- app/_Components/OurProduct/OurProduct.jsx
+++ app/_Components/OurProduct/OurProduct.jsx
@@ -37,60 +37,75 @@ const OurProduct = () => {
 
   return (
     <div>
-      <div className="text-center mt-[100px]">
-        <h1 className="text-[50px]">Our Products</h1>
-        <p className="text-[18px] text-gray-600">
+      <div className="text-center mt-[20px] md:mt-[100px]">
+        <h1 className="text-[25px] md:text-[50px]">Our Products</h1>
+        <p className="text-[16px] lg:text-[18px] text-gray-600">
           A highly efficient slip-ring scanner for today's diagnostic
           requirements.
         </p>
       </div>
       <div>
-        <div className="my-[50px] flex justify-center items-center">
+        <div className="my-[20px] md:my-[50px] flex flex-wrap gap-2 md:gap-0 justify-center items-center">
           <button
             onClick={() => setToggle(0)}
-            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+            className={`text-[14px] md:text-[18px]    pb-[4px] md:pb-3 justify-center md:w-[200px] ${
               toggle === 0 && "border-[#80b500] text-[#80b500] border-b-2"
             }`}
           >
             FOOD & DRINKS
           </button>
-          <Apple className="text-gray-400 pb-3" size={32} />
+          <Apple
+            className="text-gray-400 pb-[4px] md:pb-3 md:flex hidden"
+            size={32}
+          />
           <button
             onClick={() => setToggle(1)}
-            className={`text-[18px]   pb-3 justify-center w-[200px] ${
+            className={`text-[14px] md:text-[18px]   pb-[4px] md:pb-3 justify-center md:w-[200px] ${
               toggle === 1 && "border-[#80b500] text-[#80b500] border-b-2"
             }`}
           >
             VEGETABLES
           </button>
-          <Salad className="text-gray-400 pb-3 " size={32} />
+          <Salad
+            className="text-gray-400 pb-[4px] md:pb-3 md:flex hidden"
+            size={32}
+          />
           <button
             onClick={() => setToggle(2)}
-            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+            className={`text-[14px] md:text-[18px]    pb-[4px] md:pb-3 justify-center md:w-[200px] ${
               toggle === 2 && "border-[#80b500] text-[#80b500] border-b-2"
             }`}
           >
             DRIED FOODS
           </button>
-          <Cherry className="text-gray-400 pb-3" size={32} />
+          <Cherry
+            className="text-gray-400 pb-[4px] md:pb-3 md:flex hidden"
+            size={32}
+          />
           <button
             onClick={() => setToggle(3)}
-            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+            className={`text-[14px] md:text-[18px]    pb-[4px] md:pb-3 justify-center md:w-[200px] ${
               toggle === 3 && "border-[#80b500] text-[#80b500] border-b-2"
             }`}
           >
             BREAD & CAKE
           </button>
-          <CakeSlice className="text-gray-400 pb-3" size={32} />
+          <CakeSlice
+            className="text-gray-400 pb-[4px] md:pb-3 md:flex hidden"
+            size={32}
+          />
           <button
             onClick={() => setToggle(4)}
-            className={`text-[18px]    pb-3 justify-center w-[200px] ${
+            className={`text-[14px] md:text-[18px]    pb-[4px] md:pb-3 justify-center md:w-[200px] ${
               toggle === 4 && "border-[#80b500] text-[#80b500] border-b-2"
             }`}
           >
             FISH & MEAT
           </button>
-          <Fish className="text-gray-400 pb-3" size={32} />
+          <Fish
+            className="text-gray-400 pb-[4px] md:pb-3 md:flex hidden"
+            size={32}
+          />
         </div>
         <div className="mb-10">
           <Swiper
