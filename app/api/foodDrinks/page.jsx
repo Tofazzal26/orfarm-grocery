1827de38b138ba9b86351aaddbfcc21487937a9c
Tofazzal26 <afranislamabir6789@gmail.com>
2024-09-18 12:03:03 +0600

    news style finish

--- app/api/foodDrinks/page.jsx
+++ app/api/foodDrinks/page.jsx
@@ -7,7 +7,21 @@ const FoodDrinks = () => {
       title:
         "Adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
       paragraph:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Organic and healthy snack options like dried fruits, nuts, granola, as well as beverages like cold-pressed juices and herbal teas.",
+    },
+    {
+      image: "/News2.png",
+      title:
+        "Magna aliqua. Ut enim ad minim venia m, quis nostrud exercitation ullamco",
+      paragraph:
+        " wide range of organic dairy products such as milk butter yogurt and free-range eggs, all sourced from local farms",
+    },
+    {
+      image: "/News3.png",
+      title:
+        "Adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.",
+      paragraph:
+        "A collection of organic rice, lentils, beans, and flour, catering to health-conscious consumers",
     },
   ];
 
@@ -52,7 +66,38 @@ const FoodDrinks = () => {
           </div>
         </div>
       </div>
-      <div></div>
+      <div>
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-3 mb-8">
+          {newsDetails.map((item, idx) => (
+            <div key={idx}>
+              <div className="p-4 shadow-md dark:bg-gray-50 dark:text-gray-800 h-full">
+                <div className="space-y-4">
+                  <div className="space-y-2">
+                    <img
+                      src={item?.image}
+                      alt=""
+                      className="block object-cover object-center w-full rounded-md h-72 dark:bg-gray-500"
+                    />
+                    <div className="flex items-center text-xs">
+                      <span>6 min ago</span>
+                    </div>
+                  </div>
+                  <div className="space-y-2">
+                    <a rel="noopener noreferrer" href="#" className="block">
+                      <h3 className="text-xl font-semibold dark:text-violet-600">
+                        {item?.title}
+                      </h3>
+                    </a>
+                    <p className="leading-snug dark:text-gray-600">
+                      {item?.paragraph}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
